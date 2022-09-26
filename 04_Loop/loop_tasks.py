06c7289edcba7a14be67e3619b14318a4513d332
Abdulrahman_Elsharef <abdoelsharef1@gmail.com>
2022-09-26 14:38:03 +0200

    loop tasks

---
+++ 04_Loop/loop_tasks.py
@@ -0,0 +1,71 @@
+'''def loop(num1,num2):
+    for x in range(num1,num2+1):
+         print (x)
+num1=int(input("enter mum1 : "))
+num2=int(input("enter mum2 : "))
+loop(num1,num2) '''
+
+''' enter mum1 : 5
+enter mum2 : 17
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17 '''
+
+'''enter mum1 : 6
+enter mum2 : 17
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16'''
+
+
+
+'''def while_loop(n):
+    x=1
+    while x <=100 :
+            if x % n == 0:
+                print(x)
+            x+=1
+num=int(input("enter mum1 : "))
+while_loop(num) '''
+
+'''enter mum1 : 7
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+77
+84
+91
+98 '''
+'''enter mum1 : 15
+15
+30
+45
+60
+75
+90'''
+
